afcd4573fd9b40d2cd5c7d9ab5b842474687f319
Alex Priadko <apriadko@connecture.com>
2015-08-11 16:50:21 +0300

    fix session issue

--- src/main/java/com/priadko/arduino/util/HibernateUtil.java
+++ src/main/java/com/priadko/arduino/util/HibernateUtil.java
@@ -1,22 +1,22 @@
 package com.priadko.arduino.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final SessionFactory sessionFactory = createSessionFactory();
+    private static StandardServiceRegistry serviceRegistry;
 
-    private static SessionFactory buildSessionFactory() {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+  public static SessionFactory createSessionFactory() {
+    Configuration configuration = new Configuration();
+    configuration.configure();
+    serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+      configuration.getProperties()).build();
+    return configuration.buildSessionFactory(serviceRegistry);
+  }
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
