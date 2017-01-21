cd0efb6cda0bf6969204605219ef6a9c69aacdfb
Priadko Alex <pryadko@gmail.com>
2017-01-21 15:44:00 +0200

    remove unnecessary staff to move spring data

--- src/main/java/com/priadko/arduino/dao/impl/MeasureDaoImpl.java
+++ src/main/java/com/priadko/arduino/dao/impl/MeasureDaoImpl.java
@@ -18,7 +18,6 @@ import java.util.List;
 @Repository
 public class MeasureDaoImpl implements MeasureDao {
 
-    @Autowired
     SessionFactory sessionFactory;
 
     @Override
--- src/main/java/com/priadko/arduino/dao/impl/TypeMeasureDaoImpl.java
+++ src/main/java/com/priadko/arduino/dao/impl/TypeMeasureDaoImpl.java
@@ -14,7 +14,6 @@ import java.util.List;
 @Repository
 public class TypeMeasureDaoImpl implements TypeMeasureDao {
 
-    @Autowired
     SessionFactory sessionFactory;
 
     @Override
